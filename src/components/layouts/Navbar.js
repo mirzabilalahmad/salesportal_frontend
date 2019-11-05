871d0f46ffeb03dada25cd043b5a1a54be69e936
mirzabilal <mirza-bilal@bayt.net>
2019-11-05 12:49:49 +0500

    include assests:

---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,17 @@
+import React,{Fragment} from 'react';
+//import '../../assets../images/header.jpg';
+import '../../assets/css/App.css';
+import '../../assets/css/bootstrap.min.css';
+import '../../assets/css/select2.css';
+//import '../../assets/css/mystyle.css'
+
+const Nav = ()=>{
+    return(
+        <Fragment>
+              <div>sdahgdj</div>
+        </Fragment>
+
+       
+    )
+}
+export default Nav;
